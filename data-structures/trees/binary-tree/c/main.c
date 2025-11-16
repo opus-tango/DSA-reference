fa977566ce669dc89273705382a5d14449d78d16
Nayan Sawyer <33187059+opus-tango@users.noreply.github.com>
2025-11-16 12:39:19 -0500

    add test of binary tree and recursive print

--- data-structures/trees/binary-tree/c/main.c
+++ data-structures/trees/binary-tree/c/main.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+#include "binary-tree.h"
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+  Node* root = create_node(0);
+  root->left = create_node(1);
+  root->right = create_node(2);
+  root->left->left = create_node(3);
+  root->left->right = create_node(4);
+  root->right->left = create_node(5);
+  root->right->right = create_node(6);
+  print_node(root);
+
+  return 0;
 }
